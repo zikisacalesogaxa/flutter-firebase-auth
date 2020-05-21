0b2512e8c254dafe0e624e9956e1fcdc8d26ccd4
Zikisa Cale Sogaxa <zikisacalesogaxa@gmail.com>
2020-05-21 15:54:58 +0200

    able to add new names to list

--- lib/main.dart
+++ lib/main.dart
@@ -20,35 +20,88 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final myTextController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('User Names Vote')),
+      appBar: AppBar(title: Text('Names Vote')),
       body: _buildBody(context),
     );
   }
 
-  // body widget build that returns the list of the names
+  /// Body widget build that returns the list of the names
   Widget _buildBody(BuildContext context) {
     // TODO: get actual snapshot from Cloud Firestore
-    return StreamBuilder<QuerySnapshot>(
-      stream: Firestore.instance.collection('users').snapshots(),
-      builder: (context, snapshot) {
-        if (!snapshot.hasData) return LinearProgressIndicator();
-
-        return _buildList(context, snapshot.data.documents);
-      },
+    return Column(
+      verticalDirection: VerticalDirection.down,
+      children: [
+        StreamBuilder<QuerySnapshot>(
+          stream: Firestore.instance.collection('users').snapshots(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) return LinearProgressIndicator();
+              return _buildList(context, snapshot.data.documents);
+            },
+        ),
+        _buildForm()
+      ],
     );
   }
 
   /// Build the names in list form
   /// @param {array} - array of objects with name and vote count properties
   Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
-    return ListView(
-      padding: const EdgeInsets.only(top: 20.0),
-      children: snapshot.map((data) => UserListItem(context, data)).toList(),
+    return Expanded(
+        child: ListView(
+          padding: const EdgeInsets.only(top: 20.0),
+          children: snapshot.map((data) => UserListItem(context, data)).toList(),
+        )
     );
   }
-}
 
-/// Record class
+   /// Build form to add new names
+  Widget _buildForm() {
+    final _formKey = GlobalKey<FormState>();
+
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: <Widget>[
+          TextFormField(
+            controller: myTextController,
+            decoration: const InputDecoration(
+              hintText: 'Enter name here'
+            ),
+            validator: (value) {
+              if (value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: RaisedButton(
+              onPressed: () {
+                if (_formKey.currentState.validate()) {
+                  Firestore.instance.collection('users').add({
+                    'name': '${myTextController.text}',
+                    'votes': 0
+                  });
+                  myTextController.clear();
+                }
+              },
+              child: Text('Add name'),
+            ),
+          )
+        ],
+      )
+    );
+  }
+
+  @override
+  void dispose() {
+    myTextController.dispose();
+    super.dispose();
+  }
+}
